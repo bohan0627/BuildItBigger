7ef33d58c26edfa5aa3067202919be831f9802a0
BoHan <han.bo1@husky.neu.edu>
2018-11-04 10:48:50 -0500

    Implement the paid version

--- app/src/main/java/com/bohan/android/builditbigger/MainActivity.java
+++ app/src/main/java/com/bohan/android/builditbigger/MainActivity.java
@@ -2,6 +2,9 @@ package com.bohan.android.builditbigger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+
+        /*
+        MyCoolJokes myCoolJokes = new MyCoolJokes();
+
+        String randromJoke = myCoolJokes.getJoke();
+
+        Intent i = new Intent(this, JokesActivity.class);
+        i.putExtra("jokes",randromJoke);
+        startActivity(i);
+
+        */
+        EndpointsTask endpointsAsyncTask = new EndpointsTask();
+        endpointsAsyncTask.execute(this);
+
+    }
+
+
 }
--- app/src/paid/java/com/bohan/android/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/bohan/android/builditbigger/MainActivityFragment.java
@@ -1,6 +1,24 @@
 package com.bohan.android.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Bo Han.
  */
-public class MainActivityFragment {
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return root;
+    }
 }
