1383711b4d1b9701c101c3c5065a979027014d1f
BoHan <han.bo1@husky.neu.edu>
2018-11-04 10:39:59 -0500

    Initial commit

---
+++ app/src/free/java/com/bohan/android/builditbigger/MainActivityFragment.java
@@ -0,0 +1,7 @@
+package com.bohan.android.builditbigger;
+
+/**
+ * Created by Bo Han.
+ */
+public class MainActivityFragment {
+}
---
+++ app/src/paid/java/com/bohan/android/builditbigger/MainActivityFragment.java
@@ -0,0 +1,6 @@
+package com.bohan.android.builditbigger;
+/**
+ * Created by Bo Han.
+ */
+public class MainActivityFragment {
+}
